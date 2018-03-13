d707414fc99d60407c974cb019355e9b07006236
webz2feel <imran.ali125@yahoo.com>
2018-03-13 15:10:04 +0500

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -1,9 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed.');
 
-$config['smtp_host']        = 'mail.smtp2go.com';
-$config['smtp_user']        = 'imran.ali125@yahoo.com';
-$config['smtp_pass']        = 'NXRsdmZuMXFkOGV1';
-$config['smtp_port']        = 587;
+$config['smtp_host']        = '';
+$config['smtp_user']        = '';
+$config['smtp_pass']        = '';
+$config['smtp_port']        = ;
 $config['smtp_crypto']      = 'tls';                       // '' or 'tls' or 'ssl'
 $config['smtp_debug']      = 0;
 $config['smtp_debug_output']  = 'html' ;
